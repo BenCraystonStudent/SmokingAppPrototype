2784db44b1928eec1832a37b6a52399d8b8eb23a
BenCraystonStudent <273012.lincscollege@gmail.com>
2021-11-17 19:12:00 +0000

    Added Skybox changes
    Added Skybox changes, multiple spheres, velocity tracking, changed hand size and goodphysics script which just needs implementing

---
+++ Assets/Scripts/GoodPhysics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabVelocityTracked : XRGrabInteractable
+{
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        SetParentToXRRig();
+        base.OnSelectEntered(interactor);
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        SetParentToWorld();
+        base.OnSelectExited(interactor);
+    }
+
+    public void SetParentToXRRig()
+    {
+        transform.SetParent(selectingInteractor.transform);
+    }
+
+    public void SetParentToWorld()
+    {
+        transform.SetParent(null);
+    }
+}

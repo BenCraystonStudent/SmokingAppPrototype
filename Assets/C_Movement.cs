14be94e759a2b081e25e0ea4334a948e56855fae
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-01-26 16:15:50 +0000

    Movement on Left Stick
    Added movement on Left Stick

---
+++ Assets/C_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class C_Movement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode inputSource;
+    private Vector2 i_axis;
+    private CharacterController playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out i_axis);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(i_axis.x, 0, i_axis.y);
+
+        playerMovement.Move(direction * Time.deltaTime * speed);
+    }
+}

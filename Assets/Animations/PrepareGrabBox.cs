678e3778b1ae378d50e02b387e0f05dec2d0d67b
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-13 23:00:28 +0000

    NotGoingToWork
    Will have to be done through XRInteraction I think

---
+++ Assets/Animations/PrepareGrabBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrepareGrabBox : MonoBehaviour
+{
+    [SerializeField] private Animator mAnimator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GameController"))
+        {
+            mAnimator.SetBool("isCloseToBox", true);
+        }
+    }
+}

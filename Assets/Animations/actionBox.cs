368e33c143dd5d5f9c51f91ab69a893ed94f6447
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-16 16:10:01 +0000

    CigBoxNowOpensOnActivate

---
+++ Assets/Animations/actionBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class actionBox : MonoBehaviour
+{
+    public Animator mAnimator;
+    public GameObject cigaretteBox;
+    void Start()
+    {
+        mAnimator.SetBool("boxAction", false);
+    }
+
+    public void SetBoxAction()
+    {
+        mAnimator = cigaretteBox.GetComponent<Animator>();
+        mAnimator.SetBool("boxAction", true);
+    }
+
+    
+}

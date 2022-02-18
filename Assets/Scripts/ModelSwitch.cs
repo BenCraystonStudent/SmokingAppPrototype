3463c01b46ad89029efb0ad4afb8fd90caf45382
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-18 16:39:54 +0000

    StartedScriptingLeftHand

---
+++ Assets/Scripts/ModelSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ModelSwitch : MonoBehaviour
+{
+    public GameObject wCigarette, woCigarette;
+    public XRController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInParent<XRController>();
+        woCigarette.SetActive(true);
+        wCigarette.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary))
+        {
+            wCigarette.SetActive(true);
+            woCigarette.SetActive(false);
+        }
+    }
+}

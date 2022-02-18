eb492b3add4efc396bd065b0d311f7056c1f7701
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-18 18:38:29 +0000

    HandNowDisappearsOnButtonPress
    ProgressIGuess

--- Assets/Scripts/ModelSwitch.cs
+++ Assets/Scripts/ModelSwitch.cs
@@ -6,27 +6,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ModelSwitch : MonoBehaviour
 {
     public GameObject wCigarette, woCigarette;
-    public InputDevice controller1, controller2;
 
     // Start is called before the first frame update
 
-    private void Awake()
-    {
-        controller1 = GetComponent<InputDevice>();
-        controller2 = GetComponent<InputDevice>();
-    }
-
     void Start()
     {
-        
+        woCigarette.SetActive(true);
+        wCigarette.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeModels()
     {
-        if (controller1.TryGetFeatureValue(CommonUsages.primaryButton, out bool primary) && primary)
-        {
-            Debug.Log("Pressed!");
-        }
+        wCigarette.SetActive(true);
+        woCigarette.SetActive(false);
     }
 }

e4b47bdb18bf0e2053b0df8f00b61eba86c29fd2
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-19 18:57:11 +0000

    BoxCollidersAreGoingToBeTooPainful
    Try Vector3.distance instead

--- Assets/Scripts/ModelSwitchCollider.cs
+++ Assets/Scripts/ModelSwitchCollider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ModelSwitchCollider : MonoBehaviour
 {
     public GameObject wCigarette, woCigarette;
     // Start is called before the first frame update

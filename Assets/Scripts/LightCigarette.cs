6a708aea93d16b5752925eadb7628003dc0f179d
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-23 14:03:15 +0000

    CigaretteLighting
    But weird hand glitches, need to implement other side

--- Assets/Scripts/LightCigarette.cs
+++ Assets/Scripts/LightCigarette.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class LightCigarette : MonoBehaviour
 {
+    public MeshRenderer litCylinderMesh;
     public GameObject litCylinder;
     public ParticleSystem lighterFire;
     public bool isLit;
+    private float lighterFireDistance, heldCigaretteDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        litCylinderMesh.enabled = false;
+    }
+
+    public void LightCigaretteMethod()
+    {
+        lighterFireDistance = Vector3.Distance(lighterFire.transform.position, litCylinder.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lighterFireDistance = Vector3.Distance(lighterFire.transform.position, litCylinder.transform.position);
+        if (lighterFireDistance < 0.05f && lighterFire.isPlaying)
+        {
+            litCylinderMesh.enabled = true;
+        }
     }
 }

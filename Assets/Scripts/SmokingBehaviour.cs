b8d20bf0e4bd06027483545d74d332ac0ca49b57
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-24 14:43:34 +0000

    AddedSmokingBehaviour - UnityDidCrash
    Please don't mess up

---
+++ Assets/Scripts/SmokingBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokingBehaviour : MonoBehaviour
+{
+    public GameObject leftHand, rightHand;
+    public ParticleSystem smoke;
+    public TrailRenderer smokeTrail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        smoke.Stop();
+        smokeTrail.emitting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (leftHand.GetComponent<LightCigarette>().isLit == true || rightHand.GetComponent<LightCigarette>().isLit == true)
+        {
+            smoke.Play();
+            smokeTrail.emitting = true;
+        }
+    }
+}

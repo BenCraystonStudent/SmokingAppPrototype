ba5d6aadad9bbd0fc313a5ef110d012b266af8d6
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-28 10:02:54 +0000

    AddedCodeToInhaleExhale

--- Assets/Scripts/InhaleExhale.cs
+++ Assets/Scripts/InhaleExhale.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class InhaleExhale : MonoBehaviour
 {
+    AudioClip micAudio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        micAudio = Microphone.Start("Rift Audio", true, 3, 44100);
     }
 
     // Update is called once per frame

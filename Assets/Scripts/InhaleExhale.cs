5f0fc1ec362baed13f1a8fd11d6470da312e0985
BenCraystonStudent <273012.lincscollege@gmail.com>
2022-02-28 12:22:36 +0000

    AddedBoxHeadInhaleExhaleTracker
    This might work yet

--- Assets/Scripts/InhaleExhale.cs
+++ Assets/Scripts/InhaleExhale.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class InhaleExhale : MonoBehaviour
 {
     AudioClip micAudio;
+    public GameObject inhaleExhaleTracker, VRcamera;
     public ParticleSystem mBlownSmoke;
     // Start is called before the first frame update
     void Start()
     {
+        inhaleExhaleTracker.transform.position = VRcamera.transform.position;
+        inhaleExhaleTracker.transform.rotation = VRcamera.transform.rotation;
         micAudio = Microphone.Start("Rift Audio", true, 3, 44100);
         mBlownSmoke.Stop();
     }
@@ -16,7 +19,8 @@ public class InhaleExhale : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        inhaleExhaleTracker.transform.position = VRcamera.transform.position;
+        inhaleExhaleTracker.transform.rotation = VRcamera.transform.rotation;
     }
 
     void OnTriggerEnter(Collider other)
